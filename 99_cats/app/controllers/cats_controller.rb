b55e9c18cd4794dc7f61b2b945752bfda9580d16
Samuel Sacco <SamLSacco@aol.com>
2021-05-27 16:58:43 -0400

    cats phase II

--- 99_cats/app/controllers/cats_controller.rb
+++ 99_cats/app/controllers/cats_controller.rb
@@ -15,22 +15,31 @@ class CatsController < ApplicationController
     end
 
     def create
-        cat = Cat.new(params)
+        @cat = Cat.new(cat_params)
 
-        if cat.save
-           redirect_to cat_url(cat.id)
+        if @cat.save
+           redirect_to cat_url(@cat.id)
         else
             render :new
         end
     end
 
     def edit
-
+        @cat = Cat.find(params[:id])
+        render :edit
     end
 
     def update
+        @cat = Cat.find(params[:id])
 
+        if @cat.update(cat_params)
+           redirect_to cat_url(@cat.id)
+        else
+            render :edit
+        end
     end
 
-
+    def cat_params
+        params.require(:cat).permit(:name, :birth_date, :color, :description, :sex)
+    end
 end
