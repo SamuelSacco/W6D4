dad9c2569a03d1fccd9c3820527deb2aeff05e6d
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-05-27 15:49:11 -0400

    finished new view page

--- 99_cats/app/controllers/cats_controller.rb
+++ 99_cats/app/controllers/cats_controller.rb
@@ -9,4 +9,28 @@ class CatsController < ApplicationController
         @cat = Cat.find(params[:id])
         render :show
     end
+
+    def new
+        render :new
+    end
+
+    def create
+        cat = Cat.new(params)
+
+        if cat.save
+           redirect_to cat_url(cat.id)
+        else
+            render :new
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+
 end
--- 99_cats/config/routes.rb
+++ 99_cats/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/cats', to: 'cats#index'
-  get '/cats/:id', to: 'cats#show'
+#   get '/cats', to: 'cats#index'
+#   get '/cats/:id', to: 'cats#show'
+
+    resources :cats, only: [:index, :show, :create, :new, :update, :edit]
+
 end
